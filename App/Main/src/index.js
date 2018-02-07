7f449b2a41a32fc37a140fa5114071d62bb24982
AveCaesar <illidiant1900@gmail.com>
2018-02-07 21:30:18 +0300

    its complete

---
+++ App/Main/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Container from './components/container';
+import '.././styles/root.styl'
+
+ReactDOM.render(
+  <Container />,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,39 @@
-const path = require('path')
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-  entry: './App/index.js',
-  
-  output: {
-    path: path.resolve(__dirname, 'Dest'),
-    filename: 'bundle.js'
-  },
-  
-  module: {
-       loaders: [
-           { test:  /\.js$/,
-             loader: 'babel' }
-       ]
-   }
+module.exports = env => {
+  return {
+    entry: './App/' + env.NODE_ENV + '/src/index.js',
+
+    output: {
+      path: path.join(__dirname, '/Dest/' + env.NODE_ENV + '/'),
+      filename: '[name].bundle.js'
+    },
+    
+    module: {
+      loaders: [
+        {
+          test: /\.js|.jsx?$/,
+          exclude: /(node_modules)/,
+          loader: "babel-loader"
+        },
+        {
+          test: /\.styl$/,
+          loader: ['style-loader', 'css-loader', 'stylus-loader']
+        },
+        {
+          test: /\.pug$/,
+          loader: 'pug-loader'
+        }
+      ]
+    },
+    
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'App/' + env.NODE_ENV + '/pug/index.pug',
+        filename: 'index.html'
+      })
+    ]  
+  }
 }
