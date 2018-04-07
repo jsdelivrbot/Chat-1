cdff7792a0faccf9fea2b611a580181c8bbe5d12
AveCaesar <illidiant1900@gmail.com>
2018-04-07 20:39:38 +0300

    .

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-var express = require('express')
-var app = express()
-var path = require('path')
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
-
-let stack = {}, connected_users = {}
+var express = require('express'),
+    app = express(),
+    path = require('path'),
+    http = require('http').Server(app),
+    io = require('socket.io')(http)
+    
+var stack = {}, 
+    connected_users = {}, 
+    mutex = false
+    
+/*--------------------------------------------------------------------------------------------------*/
 
 app.use(express.static(path.join(__dirname, '/Dest')))
 
@@ -16,42 +20,41 @@ io.on('connection', (socket) => {
   })
   
   socket.on('join_queue', async (data) => {
-    stack[socket.id] = {id: socket.id, ...data}
     console.log('User joining the queue', stack)
+
+    stack[socket.id] = { id: socket.id, ...data }
+    
+    if(!mutex) {
+      mutex = true
+      await connect()
+      mutex = false
+    }
   })
   
   socket.on('disconnect', () => {
-    delete stack[socket.id]
+    delete stack[socket.id]  
+    delete connected_users[socket.id]
+    delete connected_users[connected_users[socket.id]]
     
     io.sockets.connected[connected_users[socket.id]].emit('server_allows_joining')
-    
-    delete connected_users[socket.id]
-    delete connected_users[socket.id]
-    
   })
 })
 
-const connect = () => {
-  setInterval(() => {
-    if(Object.keys(stack).length === 2){
-      let array = []
-      
-      for(key in stack) {
-        array.push(key)
-      }
-      
-      connected_users[array[0]] = array[1]
-      connected_users[array[1]] = array[0]
-      
-      delete stack[array[0]]
-      delete stack[array[1]]
-      
-      io.sockets.connected[array[0]].emit('server_allows_joining')
-      io.sockets.connected[array[1]].emit('server_allows_joining')
-    }
-    
-  }, 1000)
-}
+const connect = () => new Promise((resolve, reject) => {
+  let array = []
+  
+  for(key in stack) {
+    array.push(key)
+  }
+  
+  connected_users[array[0]] = array[1]
+  connected_users[array[1]] = array[0]
+  
+  delete stack[array[0]]
+  delete stack[array[1]]
+  
+  io.sockets.connected[array[0]].emit('server_allows_joining')
+  io.sockets.connected[array[1]].emit('server_allows_joining')
+})
 
 http.listen(process.env.PORT || 8080, () => console.log('listening on *:8080'))
-connect()
