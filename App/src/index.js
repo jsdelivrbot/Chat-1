54726e0d24b188ea35e70f58ad88ddb5f93715cc
AveCaesar <illidiant1900@gmail.com>
2018-04-26 22:38:22 +0300

    router

--- App/src/index.js
+++ App/src/index.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import { BrowserRouter, browserHistory, Switch, Route, Link } from 'react-router-dom'
 import io from 'socket.io-client'
 
 import { add } from './actions/actions'
@@ -53,34 +54,48 @@ socket.on('sending_control', async () => {
 
 /* ----------------------------- Rendering ---------------------------------- */
 
+const LinkToChat = () => (<Link to='/chat'> Link To the Chat </Link>)
+
 render(
   <Provider store = { store }>
-    <Fragment>
-      <div className = 'navbar'>
-        <img src = './chatlogos/unnamed4.png'/>
-        <HideSettings/>
-      </div>
-      
-      <div className = 'main'>
-        <div className = 'settings'>
-          <div>
-            <Gender person = 'ME'/>
-            <MyAge/>
-          </div>
-          <div>
-            <Gender/>
-            <StrangersAge/>
-          </div>
-          <Settings/>
+    <BrowserRouter history = { browserHistory }>
+     
+      <Fragment>
+        <div className = 'navbar'>
+          <img src = './chatlogos/unnamed4.png'/>
+          <HideSettings/>          
         </div>
         
-        <div className = 'conversation'>
-          <Container/>
-          <Input socket = { socket }/>
+        <div className = 'main'>
+          <div className = 'settings'>
+            <div>
+              <Gender person = 'ME'/>
+              <MyAge/>
+            </div>
+            <div>
+              <Gender/>
+              <StrangersAge/>
+            </div>
+            <Settings/>
+          </div>
+          
+          <div className = 'conversation'>
+            <Switch>
+              <Route exact path='/' component = { LinkToChat } />
+              
+              <Route path='/chat' render = {() => (
+                <Fragment>
+                  <Container/>
+                  <Input socket = { socket }/>
+                </Fragment>
+              )}/>
+              
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Fragment>
       
-    </Fragment>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('app')
 )
